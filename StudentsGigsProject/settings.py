3ffbfc9b2f3f083d318b193d9a2ffc4bc5f9f885
extechnology <extechnology.in@gmail.com>
2025-01-22 17:10:00 +0530

    update

--- StudentsGigsProject/settings.py
+++ StudentsGigsProject/settings.py
@@ -158,8 +158,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),  # Customize as needed
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),  # Customize as needed
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
